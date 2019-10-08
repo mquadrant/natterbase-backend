c3051e591286869d5e87784061224d4234e45fcc
Benjamin Mark <mquadrant@gmail.com>
2019-10-08 18:42:41 +0100

    refactored controller to validate data item

--- src/controllers/methodsController.ts
+++ src/controllers/methodsController.ts
@@ -1,15 +1,32 @@
 import { Request, Response, NextFunction } from 'express'
 import inputValidationFunction from './../functions/inputValidation'
+import { validateItem } from './../joi_validation'
 
 export const inputValidation = function(
     req: Request,
     res: Response,
     _next: NextFunction
 ) {
-    const result = inputValidationFunction(req.body.data, req.body.rules)
-    res.json({
-        data: result,
-    })
+    //joi validation
+    const { error } = validateItem(req.body)
+    if (error) {
+        // send a 422 error response if validation fails
+        return res.status(422).json({
+            status: 'error',
+            message: 'Invalid request data',
+            data: req.body,
+        })
+    } else {
+        const result = inputValidationFunction(req.body.data, req.body.rules)
+        return res.status(201).json({
+            status: 'success',
+            message: result.result,
+            data: {
+                invalidItem: result.invalidItem,
+                itemNotPresent: result.itemNotPresent,
+            },
+        })
+    }
 }
 
 export const itemRemover = async function(
