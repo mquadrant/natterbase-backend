3bc3d387829ad0b8247d38a3d4c9e4f7812dbf08
Benjamin Mark <mquadrant@gmail.com>
2019-10-08 19:18:05 +0100

    added a controllet to remove item from req body

--- src/controllers/methodsController.ts
+++ src/controllers/methodsController.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 import inputValidationFunction from './../functions/inputValidation'
-import { validateItem } from './../joi_validation'
+import itemRemoverFunction from './../functions/itemRemover'
+import { validateItem, removeItem } from './../joi_validation'
 
 export const inputValidation = function(
     req: Request,
@@ -30,10 +31,34 @@ export const inputValidation = function(
 }
 
 export const itemRemover = async function(
-    _req: Request,
-    _res: Response,
+    req: Request,
+    res: Response,
     _next: NextFunction
-) {}
+) {
+    //joi validation
+    const { error } = removeItem(req.body)
+    if (error) {
+        // send a 422 error response if validation fails
+        return res.status(422).json({
+            status: 'error',
+            message: 'Invalid request data',
+            data: req.body,
+        })
+    } else {
+        const result = itemRemoverFunction(req.body.data, req.body.item)
+        if (result.result) {
+            return res.status(201).json({
+                status: 'success',
+                data: result.message,
+            })
+        } else {
+            return res.status(201).json({
+                status: 'fail',
+                message: result.message,
+            })
+        }
+    }
+}
 
 export const aladdinTravel = async function(
     _req: Request,
