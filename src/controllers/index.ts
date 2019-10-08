b8c59cc17c934c0636692324425ed8c4c3cd8be6
Benjamin Mark <mquadrant@gmail.com>
2019-10-08 15:44:49 +0100

    created a controller for available method route

---
+++ src/controllers/index.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from 'express'
+import env from '../config'
+
+//Available methods
+export const availableMethods = async function(
+    _req: Request,
+    res: Response,
+    _next: NextFunction
+) {
+    res.json({
+        message: 'Methods available in this API',
+        apiDocUrl: '',
+        data: [
+            {
+                id: 1,
+                name: 'Input Validation',
+                description:
+                    'Function to perform input validation on a request body',
+                url: `${env.backendURL}/api/input_validation`,
+            },
+            {
+                id: 2,
+                name: 'Item Remover',
+                description: 'Function to remove an item from an object',
+                url: `${env.backendURL}/api/item_remover`,
+            },
+            {
+                id: 3,
+                name: 'Aladdin traveling carpet',
+                description:
+                    'Function to find the lowest index of the starting points that Aladdin can start his journey and be able to visit all the magical locations on his path',
+                url: `${env.backendURL}/api/aladdin_travel`,
+            },
+        ],
+    })
+}
