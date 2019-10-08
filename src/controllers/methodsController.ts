e7e40635163e1741322c9279ccce270219ffb8d3
Benjamin Mark <mquadrant@gmail.com>
2019-10-08 16:17:49 +0100

    created controller file for methods

---
+++ src/controllers/methodsController.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express'
+
+//Available methods
+export const inputValidation = async function(
+    _req: Request,
+    _res: Response,
+    _next: NextFunction
+) {}
+
+//Available methods
+export const itemRemover = async function(
+    _req: Request,
+    _res: Response,
+    _next: NextFunction
+) {}
+
+//Available methods
+export const aladdinTravel = async function(
+    _req: Request,
+    _res: Response,
+    _next: NextFunction
+) {}
