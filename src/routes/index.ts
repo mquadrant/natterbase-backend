2c4b9c88a47a8c6007449bdac38a67c1624ebe34
Benjamin Mark <mquadrant@gmail.com>
2019-10-08 13:37:08 +0100

    express refactored to typescript code

---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from 'express'
+const router = express.Router()
+
+/* GET home page. */
+router.get('/', function(_req: Request, res: Response) {
+    res.render('index', { title: 'Express' })
+})
+
+export default router
